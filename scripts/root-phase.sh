78d61d3c2c2e384362710eb1cb0d36ae15bb84a9
Clay Freeman <git@clayfreeman.com>
2025-05-12 16:56:45 -0500

    add missing package

--- scripts/root-phase.sh
+++ scripts/root-phase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-pacman -Syu --noconfirm composer curl ffmpeg jq php streamlink
+pacman -Syu --noconfirm bc composer curl ffmpeg jq php streamlink
 useradd -m -s /bin/bash -d /opt/archival archival
